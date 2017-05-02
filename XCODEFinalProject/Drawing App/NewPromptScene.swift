dfbfaa84fe9d065140c949e164cc874b36bdddb3
jpl88 <jpl88@case.edu>
2017-05-02 18:19:44 -0400

    Added Some Core Data Functionality.

--- XCODEFinalProject/Drawing App/NewPromptScene.swift
+++ XCODEFinalProject/Drawing App/NewPromptScene.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class NewPromptScene: UIViewController {
     var teamNames: [String] = []
     var turn = 5
     var scores = [0,0,0,0]
+    
+    @IBOutlet weak var PromptLabel: UILabel!
+    var prompts:[String] = []
     
     @IBOutlet weak var turntNumber: UILabel!
 
@@ -22,6 +26,39 @@ class NewPromptScene: UIViewController {
         
         // Do any additional setup after loading the view.
         turntNumber.text = String(turn)
+        
+        let appDelegate   = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        createPrompts(context: context)
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Prompts")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(request)
+            
+            if results.count > 0
+            {
+                for result in results as! [NSManagedObject]
+                {
+                    print(result)
+                    if let words = result.value(forKey: "words") as? String{
+                        prompts.append(words)
+                        print (words)
+                    }
+                }
+                PromptLabel.text = prompts[0]
+            }
+            else{
+                print("error")
+            }
+        }
+        catch{
+            
+        }
         
     }
     
@@ -54,6 +91,55 @@ class NewPromptScene: UIViewController {
         performSegue(withIdentifier: "startDrawingSegue", sender: self)
     }
     
+    //sets prompts variable of object
+    func createPrompts(context: NSManagedObjectContext){
+        let appDelegate   = UIApplication.shared.delegate as! AppDelegate
+        
+        
+        let newPrompt = NSEntityDescription .insertNewObject(forEntityName: "Prompts", into: context)
+        
+        //check if we need to add in objects
+        let testEmpty = NSFetchRequest<NSFetchRequestResult>(entityName: "Prompts")
+        
+        testEmpty.returnsObjectsAsFaults = false
+        
+        if(entityEmpty(contextUse: context, fetch: testEmpty)){
+            
+            newPrompt.setValue("Ram Ranch", forKey: "words")
+            
+            
+            do {
+                try context.save()
+                print("Saved")
+            } catch {
+                //process error
+            }
+        }
+
+    }
+    
+    //checks if entity set needs to have values added into it
+    func entityEmpty(contextUse: NSManagedObjectContext, fetch: NSFetchRequest<NSFetchRequestResult>) -> Bool{
+        do{
+            let results = try contextUse.fetch(fetch)
+            if(results.count <= 0){
+                return true
+            }
+            else{
+                for result in results as! [NSManagedObject]
+                {
+                    if result.value(forKey: "words") == nil{
+                        return true
+                    }
+                }
+                return false
+            }
+            
+        }
+        catch{
+            return true;
+        }
+    }
     
 
     /*
